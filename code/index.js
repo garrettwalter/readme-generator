154aa68cb0c8842c307ab2b7e53bbcbee8505e91
Garrett Walter <gtwalter150@gmail.com>
2020-11-03 17:12:56 -0500

    added licenses

--- code/index.js
+++ code/index.js
@@ -57,28 +57,50 @@ inquirer
     {
         type: 'list',
         message: 'What type of license would you like?',
-        choices: ['MIT','Apache 2.0','Mozilla 2.0','Common Development and Distribution License'],
+        choices: ['MIT','ISC'],
         name: 'license'
-    }
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your full name?',
+    },
   ])
   .then((data) => {
     
     let chosenLicense;
     if(data.license = 'MIT') {
-        chosenLicense = 'mit';
-    } else if(data.license = 'Apache 2.0'){
-        chosenLicense = '';
-    } else if(data.license = 'Mozilla 2.0'){
-        chosenLicense = '';
-    } else if(data.license = 'Common Development and Distribution License'){
-        chosenLicense = '';
+        chosenLicense = `Copyright <YEAR> <${data.name}>
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+        (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+        publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+        subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+        OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+        LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+        CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        
+        `;
+    } else if(data.license = 'ISC'){
+        chosenLicense = `Copyright <YEAR> <${data.name}>
+
+        Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+        provided that the above copyright notice and this permission notice appear in all copies.
+        
+        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+        IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+        INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
+        WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE 
+        OR PERFORMANCE OF THIS SOFTWARE.`;
     }
 
     const readmeTemplate = `
 #${data.title}
 
-#Table of Contents
-
+##Table of Contents
 - [Description](#Description)
 - [Installation](#Installation)
 - [Usage](#Usage)
@@ -111,6 +133,8 @@ inquirer
 
 ##License
 
+    [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+    
     ${chosenLicense}
     
 ##Questions
